18b0279ee592025a7779816d55d4f85cf7acd6a0
amir <amirkhakshour@gmail.com>
2020-09-30 10:33:05 +0200

    added unique tiny_url generation to ShortURL model

--- apps/shortner/models.py
+++ apps/shortner/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from .services import gen_random_alphanum_string
 
 
 class ShortURL(models.Model):
@@ -9,3 +10,10 @@ class ShortURL(models.Model):
     class Meta:
         verbose_name = 'URL Shortner'
         verbose_name_plural = 'URLs Shortner'
+
+    def save(self, *args, **kwargs):
+        if not self.tiny_url:
+            self.tiny_url = gen_random_alphanum_string()
+            while self.__class__.objects.filter(tiny_url=self.tiny_url).exists():
+                self.tiny_url = gen_random_alphanum_string()
+        super(ShortURL, self).save(*args, **kwargs)
