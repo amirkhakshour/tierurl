5352336537534d215d1bc253726180bef519810a
amir <amirkhakshour@gmail.com>
2020-09-30 11:26:53 +0200

    added test for checking type of the generated short url

--- apps/shortner/tests.py
+++ apps/shortner/tests.py
@@ -2,12 +2,14 @@ from django.test import TestCase
 from django.shortcuts import reverse
 from django.http.response import HttpResponseNotAllowed, HttpResponseBadRequest
 from shortner.views import MESSAGE_INVALID_DATA_PARAMS
+from shortner.models import ShortURL
 
 
 # Create your tests here.
 class ShortnerPageTestCase(TestCase):
     def setUp(self):
         self.base_url = reverse('shortner-page')
+        self.test_url = 'https://dummy.com'
 
     def test_post_is_only_allowed(self):
         response = self.client.get(self.base_url)
@@ -17,3 +19,10 @@ class ShortnerPageTestCase(TestCase):
         response = self.client.post(self.base_url)
         assert response.status_code == HttpResponseBadRequest.status_code
         assert response.content == MESSAGE_INVALID_DATA_PARAMS
+
+    def test_returns_alphanumeric_string(self):
+        response = self.client.post(self.base_url, {'url': self.test_url})
+        response_json = response.json()  # tests for json response too
+        self.assertIn('tiny_url', response_json)
+        tiny_url = response_json['tiny_url'].rsplit('/', 1)[-1]
+        assert tiny_url.isalnum()
