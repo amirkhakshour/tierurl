8bb90f41c91bc3f77f72bb553dee8bccf6bbb18d
amir <amirkhakshour@gmail.com>
2020-09-30 10:10:16 +0200

    Added unittests for shortner data parsing - added template for shortner page.

--- apps/shortner/tests.py
+++ apps/shortner/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.shortcuts import reverse
+from django.http.response import HttpResponseNotAllowed, HttpResponseBadRequest
+from shortner.views import MESSAGE_INVALID_DATA_PARAMS
+
 
 # Create your tests here.
+class ShortnerPageTestCase(TestCase):
+    def setUp(self):
+        self.base_url = reverse('shortner-page')
+
+    def test_post_is_only_allowed(self):
+        response = self.client.get(self.base_url)
+        assert response.status_code == HttpResponseNotAllowed.status_code
+
+    def test_url_in_post_data_is_mandatory(self):
+        response = self.client.post(self.base_url)
+        assert response.status_code == HttpResponseBadRequest.status_code
+        assert response.content == MESSAGE_INVALID_DATA_PARAMS
--- apps/shortner/views.py
+++ apps/shortner/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseNotAllowed, HttpResponseBadRequest
 
-# Create your views here.
+MESSAGE_INVALID_DATA_PARAMS = b'please send url parameter in your request data.'
+
 
 def url_shortner_page(request):
-    pass
+    """URL shortner page
+    A better option would be to use DJRF serializers and DJRF APiView,
+    to remove unnecessary tests, specially validation tests.
+    """
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(['POST'])
+    if request.POST.get('url', None) is None:
+        return HttpResponseBadRequest(MESSAGE_INVALID_DATA_PARAMS)
 
+    template = 'shortner/shortner_page.html'
+    context = {}
+    return render(request, template, context)
